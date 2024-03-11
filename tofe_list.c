5cdd3304f2a8ef90fed01cba75d0376058b375c5
Jaakko Julin <jaakko.julin@jyu.fi>
2024-03-11 13:29:42 +0200

    tofe_list more header parsing etc

--- tofe_list.c
+++ tofe_list.c
@@ -3,55 +3,16 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <jibal.h>
-
-typedef enum scatter_type {
-    SCATTER_NONE = 0,
-    SCATTER_ERD = 1,
-    SCATTER_RBS = 2
-} scatter_type;
-
-typedef struct tofe_list_event {
-    int tof;
-    int e;
-    int ang;
-    int eventno;
-    double energy;
-    double angle1;
-    double angle2;
-} tofe_list_event;
-
-typedef struct cutfile {
-    char *filename;
-    size_t n_counts;
-    scatter_type type;
-    double beam_energy;
-    jibal_isotope *incident;
-    double event_weight;
-    int lineno;
-} cutfile;
-
-typedef struct list_files {
-    cutfile *cutfiles;
-    size_t n_files;
-} list_files;
-
-typedef enum {
-    CUTFILE_HEADER_NONE = 0,
-    CUTFILE_HEADER_TYPE = 1
-} cutfile_header_type;
-
-static const jibal_option cutfile_headers[] = {
-        {JIBAL_OPTION_STR_NONE, CUTFILE_HEADER_NONE},
-        {"type",                CUTFILE_HEADER_TYPE},
-        {NULL,                  0}
-};
-
-typedef enum tofe_list_msg_level {
-    TOFE_LIST_NORMAL = 0,
-    TOFE_LIST_INFO = 1,
-    TOFE_LIST_WARNING = 2,
-    TOFE_LIST_ERROR = 3
-} tofe_list_msg_level;
+#ifdef WIN32
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#include <windows.h>
+#else
+#include <libgen.h> /* for basename() */
+#include <sys/param.h> /* for MAXPATHLEN */
+#endif
+#include "tofe_list.h"
 
 const char *msg_level_str(tofe_list_msg_level level) {
     switch(level) {
@@ -82,7 +43,7 @@ void tofe_files_free(list_files *files) {
         return;
     }
     for(size_t i = 0; i < files->n_files; i++) {
-        free(files->cutfiles[i].filename);
+        cutfile_free(&files->cutfiles[i]);
     }
     free(files->cutfiles);
     free(files);
@@ -93,9 +54,10 @@ int cutfile_read_headers(cutfile *cutfile) {
     size_t line_size = 0;
     FILE *f = fopen(cutfile->filename, "r");
     if(!f) {
-        fprintf(stderr, "Could not open file \"%s\"", cutfile->filename);
+        tofe_list_msg(TOFE_LIST_ERROR, "Could not open file \"%s\"", cutfile->filename);
         return -1;
     }
+    cutfile->basename = tofe_basename(cutfile->filename);
     int error = 0;
     while(getline(&line, &line_size, f) > 0) {
         cutfile->lineno++;
@@ -120,17 +82,64 @@ int cutfile_read_headers(cutfile *cutfile) {
             continue;
         }
         //fprintf(stderr, "%i: \"%s\" and \"%s\"\n", cutfile->lineno, line, line_data);
+        char *tmp;
+        switch (jibal_option_get_value(cutfile_headers, line)) {
+            case CUTFILE_HEADER_NONE:
+                break;
+            case CUTFILE_HEADER_TYPE:
+                cutfile->type = jibal_option_get_value(tofe_scatter_types, line_data);
+                break;
+            case CUTFILE_HEADER_COUNT:
+                cutfile->n_counts = strtol(line_data, NULL, 10);
+                break;
+            case CUTFILE_HEADER_WEIGHT:
+                cutfile->event_weight = strtod(line_data, &tmp);
+                if(*tmp != '\0') {
+                    tofe_list_msg(TOFE_LIST_WARNING, "Trailing characters in event weight: %s (got %g), was conversion successful?", line_data, cutfile->event_weight);
+                }
+                break;
+            case CUTFILE_HEADER_ENERGY:
+                break;
+            case CUTFILE_HEADER_ANGLE:
+                break;
+            case CUTFILE_HEADER_ELEMENT:
+                break;
+            case CUTFILE_HEADER_LOSSES:
+                break;
+            case CUTFILE_HEADER_SPLIT:
+                break;
+            default:
+                tofe_list_msg(TOFE_LIST_WARNING, "File %s line %i: unknown header \"%s\"", cutfile->filename, cutfile->lineno, line);
+        }
     }
     fclose(f);
     return error;
 }
 
-list_files *tofe_files_allocate_from_argv(int argc, char **argv) {
+void cutfile_reset(cutfile *cutfile) {
+    free(cutfile->filename);
+    cutfile->filename = NULL;
+    cutfile->n_counts = 0;
+    cutfile->event_weight = 1.0;
+    cutfile->type = SCATTER_ERD;
+}
+
+void cutfile_free(cutfile *cutfile) {
+    free(cutfile->filename);
+    free(cutfile->basename);
+}
+
+list_files *tofe_files_from_argv(int argc, char **argv) { /* Argument vector should contain filenames. Reads headers, closes files. */
+    if(argc == 0 || argv == NULL) {
+        tofe_list_msg(TOFE_LIST_ERROR, "No files.");
+        return NULL;
+    }
     list_files *files = malloc(sizeof(list_files));
     files->n_files = argc;
     files->cutfiles = calloc(files->n_files, sizeof(cutfile));
     for(int i = 0; i < argc; i++) {
         cutfile *cutfile = &files->cutfiles[i];
+        cutfile_reset(cutfile);
         cutfile->filename = strdup(argv[i]);
         if(cutfile_read_headers(cutfile)) { /* Encountered some error, cleanup and return */
             tofe_files_free(files);
@@ -140,6 +149,38 @@ list_files *tofe_files_allocate_from_argv(int argc, char **argv) {
     }
     return files;
 }
+
+char *tofe_basename(const char *path) { /*  e.g. /bla/bla/tofe2363.O.ERD.0.cut => tofe2363.O.ERD.0.cut  */
+#ifdef WIN32
+    char *fname = malloc(_MAX_FNAME);
+    char *fname_orig = fname;
+    char *ext = malloc(_MAX_EXT);
+    _splitpath(path, NULL, NULL, fname, ext);
+    char *out = malloc(_MAX_FNAME+_MAX_EXT);
+    *out = '\0';
+    strcat(out, fname);
+    strcat(out, ext);
+    free(fname_orig);
+    free(ext);
+    return out;
+#else
+    char *bname = malloc(MAXPATHLEN);
+    basename_r(path, bname);
+    return bname;
+#endif
+}
+
+void tofe_files_print(list_files *files) {
+    if(!files) {
+        return;
+    }
+    for(size_t i = 0; i < files->n_files; i++) {
+        cutfile *cutfile = &files->cutfiles[i];
+        fprintf(stderr, "%32s | %3s | %7.4lf | %8zu\n", cutfile->basename, tofe_scatter_types[cutfile->type].s, cutfile->event_weight, cutfile->n_counts);
+    }
+}
+
+
 
 int main(int argc, char **argv) {
 #if 0
@@ -153,7 +194,11 @@ int main(int argc, char **argv) {
     }
     argc--;
     argv++;
-    list_files *files = tofe_files_allocate_from_argv(argc, argv);
+    list_files *files = tofe_files_from_argv(argc, argv);
+    if(!files) {
+        return EXIT_FAILURE;
+    }
+    tofe_files_print(files);
     tofe_files_free(files);
-    return 0;
+    return EXIT_SUCCESS;
 }
